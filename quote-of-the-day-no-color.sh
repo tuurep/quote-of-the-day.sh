c9a9455e01fd2c612f29ef2203521ed141797a82
Tuure <tuure.piitulainen@gmail.com>
2021-04-25 00:37:27 +0300

    Reduce escapes

--- quote-of-the-day-no-color.sh
+++ quote-of-the-day-no-color.sh
@@ -2,5 +2,5 @@
 
 wget -q http://www.brainyquote.com/link/quotebr.js -O - \
     | sed -n 3,4p \
-    | sed -e "1s/.*(\"\(.*\)<br>.*/\"\1\"/g" \
-          -e "2s/.*>\(.*\)<.*/\t\t--\1/g"
+    | sed -e '1s/.*("\(.*\)<br>.*/"\1"/g' \
+          -e '2s/.*>\(.*\)<.*/\t\t--\1/g'
--- quote-of-the-day.sh
+++ quote-of-the-day.sh
@@ -6,7 +6,7 @@ reset=$'\e[0m'
 
 wget -q http://www.brainyquote.com/link/quotebr.js -O - \
     | sed -n 3,4p \
-    | sed -e "1s/.*(\"\(.*\)<br>.*/\"\1\"/g" \
-          -e "2s/.*>\(.*\)<.*/\t\t--\1/g" \
+    | sed -e '1s/.*("\(.*\)<br>.*/"\1"/g' \
+          -e '2s/.*>\(.*\)<.*/\t\t--\1/g' \
           -e "1s/.*/$cyan&$reset/" \
           -e "2s/.*/$magenta&$reset/"
