4abe801ccfb7706be0923731295d9886b319efc9
Tuure <tuure.piitulainen@gmail.com>
2021-09-09 23:03:28 +0300

    Change wget to curl and fix address
    Since recently, wget outputs 'SSL_INIT', which is not disabled by the
    --quiet flag. Switching to curl is one workaround, and it may be
    better suited for this script otherwise too.

--- quote-of-the-day-no-color.sh
+++ quote-of-the-day-no-color.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget -q http://www.brainyquote.com/link/quotebr.js -O - \
+curl -s https://www.brainyquote.com/link/quotebr.js \
     | sed -n 3,4p \
     | sed -e '1s/.*("\(.*\)<br>.*/"\1"/g' \
           -e '2s/.*>\(.*\)<.*/\t\t--\1/g'
--- quote-of-the-day.sh
+++ quote-of-the-day.sh
@@ -4,7 +4,7 @@ cyan=$'\e[36m'
 magenta=$'\e[35m'
 reset=$'\e[0m'
 
-wget -q http://www.brainyquote.com/link/quotebr.js -O - \
+curl -s https://www.brainyquote.com/link/quotebr.js \
     | sed -n 3,4p \
     | sed -e '1s/.*("\(.*\)<br>.*/"\1"/g' \
           -e '2s/.*>\(.*\)<.*/\t\t--\1/g' \
